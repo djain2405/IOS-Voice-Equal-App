62b6cb7e4ff14b5bc309177c03e77814f5770cf2
Divya Jain <divyajain2405@gmail.com>
2021-02-11 23:08:01 -0800

    Voice Equal IOS initial commit with swiftUI

---
+++ Voice Equal/Voice_EqualApp.swift
@@ -0,0 +1,17 @@
+//
+//  Voice_EqualApp.swift
+//  Voice Equal
+//
+//  Created by Divya Jain on 2/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Voice_EqualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
