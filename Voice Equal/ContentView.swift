750a88f4546ddd05610a6e4ecff108ee1a184c4c
Divya Jain <divyajain2405@gmail.com>
2021-02-12 00:02:57 -0800

    Initial UI attempt and I like it so far

--- Voice Equal/ContentView.swift
+++ Voice Equal/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var womenCount: Int = 0
+    @State private var menCount: Int = 0
+
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("NUMBER OF PEOPLE")
+            HStack{
+                HStack{
+                Text("WOMEN:")
+                TextField(
+                    "",
+                    value: $womenCount,
+                    formatter: NumberFormatter()
+                ).background(Color.gray).padding(.leading , 10).padding(.trailing, 10).fixedSize(horizontal: true, vertical: false)
+                }
+                Spacer()
+                Divider().fixedSize(horizontal: true, vertical: true)
+                Spacer()
+                HStack{
+                Text("MEN:")
+                TextField(
+                    "",
+                    value: $menCount,
+                    formatter: NumberFormatter()
+                ).background(Color.gray).padding(.leading , 10).padding(.trailing, 10).fixedSize(horizontal: true, vertical: false)
+                }
+            }
+            Button(action: {
+                print("Clicked on Button")
+            }) {
+                Image(systemName: "trash")
+                    .font(.subheadline)
+                    .foregroundColor(.red)
+            }
+        }
     }
 }
 
