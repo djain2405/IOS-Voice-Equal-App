4308c19f9069cd4f1e3213d0e5b7bb4ecbe94ac2
Divya Jain <divyajain2405@gmail.com>
2021-02-13 13:04:06 -0800

    Count View Extracted - still needs warning solving

--- Voice Equal/ContentView.swift
+++ Voice Equal/ContentView.swift
@@ -8,43 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var womenCount: Int = 0
-    @State private var menCount: Int = 0
-
 
     var body: some View {
-        VStack {
-            Text("NUMBER OF PEOPLE")
-            HStack{
-                HStack{
-                Text("WOMEN:")
-                TextField(
-                    "",
-                    value: $womenCount,
-                    formatter: NumberFormatter()
-                ).background(Color.gray).padding(.leading , 10).padding(.trailing, 10).fixedSize(horizontal: true, vertical: false)
-                }
-                Spacer()
-                Divider().fixedSize(horizontal: true, vertical: true)
-                Spacer()
-                HStack{
-                Text("MEN:")
-                TextField(
-                    "",
-                    value: $menCount,
-                    formatter: NumberFormatter()
-                ).background(Color.gray).padding(.leading , 10).padding(.trailing, 10).fixedSize(horizontal: true, vertical: false)
-                }
-            }
-            Button(action: {
-                print("Clicked on Button")
-            }) {
-                Image(systemName: "trash")
-                    .font(.subheadline)
-                    .foregroundColor(.red)
-            }
-        }
+        VStack{
+            Text("DUAL TIMER").padding(10)
+            CountView().body.shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                .border(Color.gray)
+                .background(Color.yellow)
+            Spacer()
+        }.padding(24)
+        
     }
 }
 
---
+++ Voice Equal/CountView.swift
@@ -0,0 +1,62 @@
+//
+//  CountView.swift
+//  Voice Equal
+//
+//  Created by Divya Jain on 2/13/21.
+//
+
+import SwiftUI
+
+struct CountView: View {
+    @State private var womenCount: Int = 0
+    @State private var menCount: Int = 0
+    
+    var body: some View {
+        VStack {
+            Text("NUMBER OF PEOPLE")
+            HStack{
+                HStack{
+                Text("WOMEN:")
+                    .padding(.top, 16)
+                    .padding(.bottom, 16)
+                TextField(
+                    "",
+                    value: $womenCount,
+                    formatter: NumberFormatter()
+                )
+                .background(Color.gray).padding(.leading , 10)
+                .padding(.trailing, 10)
+                .padding(.top, 16)
+                .padding(.bottom, 16).fixedSize(horizontal: true, vertical: false)
+                }
+                Spacer()
+                Divider().fixedSize(horizontal: true, vertical: true)
+                Spacer()
+                HStack{
+                Text("MEN:")                    .padding(.top, 16)
+                    .padding(.bottom, 16)
+                TextField(
+                    "",
+                    value: $menCount,
+                    formatter: NumberFormatter()
+                ).background(Color.gray).padding(.leading , 10)
+                .padding(.trailing, 10)                .padding(.top, 16)
+                .padding(.bottom, 16).fixedSize(horizontal: true, vertical: false)
+                }
+            }
+            Button(action: {
+                print("Clicked on Button")
+            }) {
+                Image(systemName: "trash")
+                    .font(.subheadline)
+                    .foregroundColor(.red)
+            }
+        }.padding()
+    }
+}
+
+struct CountView_Previews: PreviewProvider {
+    static var previews: some View {
+        CountView()
+    }
+}
