4e1b2048e662a246d0ba3ac9d8a7e806b2a34cb9
Divya Jain <divyajain2405@gmail.com>
2021-02-15 00:45:08 -0800

    Update timerview for reset and duration as well

--- Voice Equal/TimerView.swift
+++ Voice Equal/TimerView.swift
@@ -9,50 +9,65 @@ import SwiftUI
 
 struct TimerView: View {
     var body: some View {
-        HStack{
-            Spacer()
-            VStack{
-                Text("WOMEN")
-                    .padding(.bottom, 8)
-                Text("00:00")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                Button(action: {
-                    print("Start Timer!")
-                }, label: {
-                    Text("Start Timer")
-                        .foregroundColor(Color.white)
-                })
-                .padding(8)
-                .background(Color("women"))
+        VStack{
+            HStack{
+                Spacer()
+                VStack{
+                    Text("WOMEN")
+                        .padding(.bottom, 8)
+                    Text("00:00")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    Button(action: {
+                        print("Start Timer!")
+                    }, label: {
+                        Text("Start Timer")
+                            .foregroundColor(Color.white)
+                    })
+                    .padding(8)
+                    .background(Color("women"))
+                }
+                .padding(24)
+                .cornerRadius(100)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 100)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+                Spacer()
+                VStack {
+                    Text("MEN")
+                        .padding(.bottom, 8)
+                    Text("00:00")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    Button(action: {
+                        print("Start Timer!")
+                    }, label: {
+                        Text("Start Timer")
+                            .foregroundColor(Color.white)
+                    })
+                    .padding(8)
+                    .background(Color("men"))
+                }
+                .padding(24)
+                .cornerRadius(100)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 100)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+                Spacer()
             }
-            .padding(24)
-            .cornerRadius(100)
-            .overlay(
-                RoundedRectangle(cornerRadius: 100)
-                    .stroke(Color.gray, lineWidth: 1)
-            )
-            Spacer()
-            VStack {
-                Text("MEN")
-                    .padding(.bottom, 8)
-                Text("00:00")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                Button(action: {
-                    print("Start Timer!")
-                }, label: {
-                    Text("Start Timer")
-                        .foregroundColor(Color.white)
-                })
-                .padding(8)
-                .background(Color("men"))
+            Text("Reset")
+            Button(action: {
+                print("clicked the reset button")
+            }) {
+                Image(systemName: "arrow.clockwise.circle")
+                    .resizable()
+                    .foregroundColor(Color.gray)
+                    .frame(width: 32, height: 32, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             }
-            .padding(24)
-            .cornerRadius(100)
-            .overlay(
-                RoundedRectangle(cornerRadius: 100)
-                    .stroke(Color.gray, lineWidth: 1)
-            )
-            Spacer()
+            Text("Total Duration")
+                .font(.caption)
+            Text("00:00")
+                .font(.headline)
         }
     }
 }
