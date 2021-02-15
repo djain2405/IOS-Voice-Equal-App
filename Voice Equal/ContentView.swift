debf5ff6013e3fcf4f758542cf282de6aecabb3f
Divya Jain <divyajain2405@gmail.com>
2021-02-15 01:03:44 -0800

    Able to navigate to result View

--- Voice Equal/ContentView.swift
+++ Voice Equal/ContentView.swift
@@ -9,26 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var resultView = false
 
     var body: some View {
-        
-        VStack{
-            Text("VOICE EQUAL")
-                .font(.headline)
-                .padding(.horizontal)
-                .padding(.top, 32)
-                .padding(.bottom, 32)
-            CounterView()
-                .padding()
-                .border(Color.gray)
-            TimerView()
-                .padding(.top, 32)
-            Spacer()
+        if(!self.resultView) {
+            return
+                AnyView(VStack{
+                Text("VOICE EQUAL")
+                    .font(.headline)
+                    .padding(.horizontal)
+                    .padding(.top, 32)
+                    .padding(.bottom, 32)
+                CounterView()
+                    .padding()
+                    .border(Color.gray)
+                TimerView()
+                    .padding(.top, 32)
+                Spacer()
+                Button(action: {
+                    withAnimation{
+                        self.resultView.toggle()
+                    }
+                    print("See Result!")
+                }, label: {
+                    Text("See Result")
+                })
+            }
+            .padding(.horizontal, 24)
+            .padding(.vertical, 16)
+            .background(Color("background"))
+            .ignoresSafeArea())
+        } else {
+            return AnyView(ResultView())
         }
-        .padding(.horizontal, 24)
-        .padding(.vertical, 16)
-        .background(Color("background"))
-        .ignoresSafeArea()
+        
+
     }
 }
 
