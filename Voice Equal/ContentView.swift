59683cec9253e28b45b385dccc040766abf953fd
Divya Jain <divyajain2405@gmail.com>
2021-02-14 18:31:50 -0800

    Yay it all works the initial counter UI

--- Voice Equal/ContentView.swift
+++ Voice Equal/ContentView.swift
@@ -9,16 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     
-//    @State private var womenCount: Int = 0
-//    @State private var menCount: Int = 0
+    @State private var womenCount: Int = 0
+    @State private var menCount: Int = 0
 
 
     var body: some View {
         
         VStack{
             Text("DUAL TIMER")
-                .font(.title)
+                .font(.headline)
                 .padding(.horizontal)
+                .padding(.top, 24)
             VStack {
                 Text("NUMBER OF PEOPLE")
                     .multilineTextAlignment(.center)
@@ -28,69 +29,32 @@ struct ContentView: View {
                     HStack{
                         Text("WOMEN:")
                             .padding(8.0)
+                        TextField("", value: $womenCount, formatter: NumberFormatter())
+                            .frame(width: 32)
                     }
                     Spacer()
                     HStack{
                         Text("MEN:")
                             .padding(8.0)
+                        TextField("", value: $menCount, formatter: NumberFormatter()).frame(width: 32)
                     }
                 }
+                Button(action: {
+                    print("clicked the button")
+                }) {
+                    Image(systemName: "pencil.circle")
+                        .resizable()
+                        .foregroundColor(Color.gray)
+                        .frame(width: 32, height: 32, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                }
             }.padding()
-            .shadow(radius: 10)
             .border(Color.gray)
             Spacer()
-        }.padding(.horizontal, 24)
-        
-        
-        
-        
-        
-//        VStack{
-//            Text("DUAL TIMER").padding(10)
-//        VStack {
-//            Text("NUMBER OF PEOPLE")
-//            HStack{
-//                HStack{
-//                Text("WOMEN:")
-//                    .padding(.top, 16)
-//                    .padding(.bottom, 16)
-//                TextField(
-//                    "",
-//                    value: $womenCount,
-//                    formatter: NumberFormatter()
-//                )
-//                .background(Color.gray).padding(.leading , 10)
-//                .padding(.trailing, 10)
-//                .padding(.top, 16)
-//                .padding(.bottom, 16).fixedSize(horizontal: true, vertical: false)
-//                }
-//                Spacer()
-//                Divider().fixedSize(horizontal: true, vertical: true)
-//                Spacer()
-//                HStack{
-//                Text("MEN:")                    .padding(.top, 16)
-//                    .padding(.bottom, 16)
-//                TextField(
-//                    "",
-//                    value: $menCount,
-//                    formatter: NumberFormatter()
-//                ).background(Color.gray).padding(.leading , 10)
-//                .padding(.trailing, 10)                .padding(.top, 16)
-//                .padding(.bottom, 16).fixedSize(horizontal: true, vertical: false)
-//                }
-//            }
-//            Button(action: {
-//                print("Clicked on Button")
-//            }) {
-//                Image(systemName: "trash")
-//                    .font(.subheadline)
-//                    .foregroundColor(.red)
-//            }
-//        }.padding().shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-//        .border(Color.gray)
-//        .background(Color.yellow)
-//            Spacer()
-//        }.padding(24)
+        }
+        .padding(.horizontal, 24)
+        .padding(.vertical, 16)
+        .background(Color("background"))
+        .ignoresSafeArea()
     }
 }
 
