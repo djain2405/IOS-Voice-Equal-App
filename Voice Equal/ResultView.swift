fe051e3a8a2ac32decdaccd5edef7482009f15d1
Divya Jain <divyajain2405@gmail.com>
2021-02-16 22:59:01 -0800

    Minor issue with background color but text for percentage works

--- Voice Equal/ResultView.swift
+++ Voice Equal/ResultView.swift
@@ -11,23 +11,44 @@ struct ResultView: View {
     @Environment(\.presentationMode) var mode: Binding<PresentationMode>
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            VStack{
+               Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                VStack(alignment: .leading){
+                    Text("WOMEN")
+                        .bold()
+                        .foregroundColor(Color("women"))
+                        .padding(.bottom, 8)
+                    Text("50% of speakers were women and they spoke 39% of the time")
+                        .padding(.bottom, 12)
+                    Text("MEN")
+                            .bold()
+                            .foregroundColor(Color("men"))
+                            .padding(.bottom, 8)
+                    Text("50% of speakers were men and they spoke 39% of the time")
+                        .padding(.bottom, 12)
+                }
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+            }
+            .padding()
             .navigationBarBackButtonHidden(true)
             .navigationBarTitle("RESULT", displayMode: .inline)
             .navigationBarItems(leading: Button(action : {
                 self.mode.wrappedValue.dismiss()
-            }){
-                Image(systemName: "arrow.left")
-            }
+                }){
+                    Image(systemName: "arrow.left")
+                }
             .padding(12)
             .overlay(
                 RoundedRectangle(cornerRadius: 144)
                     .stroke(Color.gray, lineWidth: 1)
+                )
             )
-            
-            )
-
-        
+            .background(Color("background"))
+            .ignoresSafeArea()
     }
 }
 
